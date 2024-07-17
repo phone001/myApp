0f0f298db534bcb9a72a5ad5b5883dfeaafb1a16
gyeon <https://github.com/phone001>
2024-07-17 09:33:18 +0900

    test

---
+++ myApp/README.md
@@ -0,0 +1,6 @@
+# AWS
+## 포트 포워딩
+## 프록시 설정 nginx
+## CI/CD gitHubActions
+
+### 포트 포워딩
---
+++ myApp/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("내가 보임?");
+})
+
+app.listen(3000, () => {
+    console.log("server on");
+})
